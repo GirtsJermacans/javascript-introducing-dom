71ac75656f65b1056394e7251c5b8fcd5c9f62d8
GirtsJermacans <girts.jermacans@gmail.com>
2020-11-17 00:00:36 +0000

    did not work

--- js/main.js
+++ js/main.js
@@ -9,7 +9,7 @@
     console.dir(document.getElementById("myHeading")) // looks up all properties and methodes for that element
     document.getElementById("myHeading").innerHTML = "Girts Jermacans"
 
-    document.querySelector("nav ul li").setAttribute("class", "currentPage") // finds first that match nav ul li - added css class "currentPage" to html tag
+    // document.querySelector("nav ul li").setAttribute("class", "currentPage") // finds first that match nav ul li - added css class "currentPage" to html tag
    
     // JS is event driven scripting language
     // inline events - onclick="do something" or onload="init()"
@@ -70,6 +70,45 @@
             bodyElement.setAttribute("class", colourPicked)
         }
     }
+
+    // image rotator
+    var imageAr = [
+        'images/view1.jpg',
+        'images/view2.jpg', 
+        'images/view3.jpg', 
+        'images/view4.jpg', 
+        'images/view5.jpg', 
+        'images/view6.jpg'
+    ];
+
+    var counter = 0;
+    changeImage()
+    setInterval(changeImage, 2000)
+    function changeImage(){
+        counter++
+        if (counter === imageAr.length){
+            counter = 0
+        }
+        document.getElementById("myImages").setAttribute("src", imageAr[counter])
+    }
+
+    // console.dir(window.location.href)
+    var url = window.location.href
+    // console.dir(url.split("/").pop()) // pop gets the last value from array
+    var fileName = url.split("/").pop()
+    console.info(fileName)
+
+    // Challenge 4
+    var allLinks = document.querySelectorAll('nav ul li')
+    console.info(allLinks)
+    // lets retrieve the href value of each a link and compare it to window .html value - where is a match - apply right stuff
+    console.dir(document.querySelector("a").attributes[0].nodeValue)
+   /* for (var i = 0; i < allLinks.length; i++){
+        var value = document.querySelector("a").attributes.nodeValue
+        if (value === fileName){
+            document.querySelector(allLinks[0]).setAttribute("class", "currentPage")
+        }
+    } CANT DO IT :(((( */ 
 
     // end
 })();
